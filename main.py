4942002a685bc5fd551b45b80f0a2fbc1a187e00
seqqueiralourdes <seqqueiralourdes@gmail.com>
2026-05-01 11:59:12 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def country_app():
+    
+    datos = {
+        "avisos": [
+            "Corte de agua mañana a las 8hs",
+            "Reunión de consorcio el viernes",
+            "Mantenimiento de espacios verdes el lunes"
+        ],
+        "expensas": {
+            "estado": "Al día",
+            "monto": 15000,
+            "vencimiento": "10/05/2026"
+        }
+    }
+
+    response = jsonify(datos)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True)
